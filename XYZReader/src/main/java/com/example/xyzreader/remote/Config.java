381c8c9180073c71474b6a70efe1f5f8fa7abc2e
artem-p <artempugachev1@gmail.com>
2017-11-27 13:14:43 +0300

    add min width to article list item

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -12,7 +12,10 @@ public class Config {
     static {
         URL url = null;
         try {
-            url = new URL("https://nspf.github.io/XYZReader/data.json");
+            // https://nspf.github.io/XYZReader/data.json
+            // https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json
+            // https://raw.githubusercontent.com/artem-p/XYZReader/master/xyz.json
+            url = new URL("https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json");
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
             Log.e(TAG, "Please check your internet connection.");
